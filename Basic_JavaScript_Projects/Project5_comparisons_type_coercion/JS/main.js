19a3d08afb9217309ea4facc323ff3aa79a064f7
Chris Chamblin <chamblin145@gmail.com>
2021-04-06 21:52:48 -0700

    Commit

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,21 @@
+document.write(typeof "Word"); //Gives the data type of a variable which would be a string here
+
+document.write(typeof 3); //Gives the data type of a variable which would be a number here
+
+document.write("80" + 6);
+
+function my_Function()  {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+document.getElementById("Test2").innerHTML = isNaN('This is a string');
+
+document.getElementById("Test3").innerHTML = isNaN('007');
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10>2);
+
+document.write(10<2);
