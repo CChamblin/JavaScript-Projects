c86381983a9a9b9dcb7c13688a3cbb7be9ff63c5
Chris Chamblin <chamblin145@gmail.com>
2021-04-10 22:30:19 -0700

    Commit

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -8,7 +8,7 @@ function my_Function()  {
     document.getElementById("Test").innerHTML = 0/0;
 }
 
-document.getElementById("Test2").innerHTML = isNaN('This is a string');
+document.getElementById("Test2").innerHTML = isNaN("This is a string");
 
 document.getElementById("Test3").innerHTML = isNaN('007');
 
@@ -19,3 +19,39 @@ document.write(-3E310);
 document.write(10>2);
 
 document.write(10<2);
+
+console.log(6*4);
+
+document.write(10 == 10);
+
+document.write(3 == 11);
+
+X = 10;
+Y = 10;
+document.write(X ===Y);
+
+X = 82;
+Y = "82";
+document.write(X === Y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A ===B);
+
+//Boolean operators//
+document.write(5>2 && 10>4); //AND operator that results in TRUE//
+
+document.write(5>10 && 10>4); //AND operator that results in FALSE//
+
+document.write(5>10 || 10>4); //OR operator that results in TRUE//
+
+document.write(5>10 || 10>20); //OR operator that results in FALSE//
+
+function not_Fucntion()  {
+    document.getElementByID("Not").innerHTML = !(20>10);
+}
+
+function not_Fucntion()  {
+    document.getElementByID("Not").innerHTML = !(5>10);
+}
+
