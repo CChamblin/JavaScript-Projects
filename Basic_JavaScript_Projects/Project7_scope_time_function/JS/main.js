eb168eeb4184961093a26e8b5bd8aafebedae901
Chris Chamblin <chamblin145@gmail.com>
2021-05-01 15:52:43 -0700

    Project 8

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -75,12 +75,3 @@ function Time_function()  {
     }
     document.getElementById("Time_of_day").innerHTML = Reply;
 }
-
-function full_Sentence()  {
-    var part_1 = "I have ";
-    var part_2 = "made this ";
-    var part_3 = "into a complete ";
-    var part_4 = "sentence.";
-    var whole_sentence = part_1.concat(part_2, part_3, part_4);
-    document.getElementById("Concatenate").innerHTML = whole_sentence;
-}
