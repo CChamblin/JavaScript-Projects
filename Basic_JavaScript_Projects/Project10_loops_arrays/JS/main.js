edc55b95a55227093793b41ad14943b539cd3e98
Chris Chamblin <chamblin145@gmail.com>
2021-06-29 23:02:44 -0700

    Project 10
    Can't debug the return statement example from W3. Losing something in the translation from HTML to JS.

---
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,59 @@
+function myFunction()  {
+    var str = "Hello World!";
+    var n = str.length;
+    document.getElementById("demo").innerHTML = n;        
+    }
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop()  {
+    for (Y = 0; Y < Instruments.length; Y++)  {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function cat_pics() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
+}
+
+function constant_function() {
+    const Pet_Dog = {size:"medium", breed:"Bulldog", color:"grey"};
+    Pet_Dog.breed = "Greyhound";
+    Pet_Dog.tempermant = "playful";
+    document.getElementById("Constant").innerHTML = "The tempermant of the " +
+        Pet_Dog.breed + " is " + Pet_Dog.tempermant;
+}
+
+//---EXAMPLES--//
+
+var  X=82;
+document.write(X);
+{
+    let X=33;   // example of let keyword and block scope which limits access to inside the block
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+
+var  X=82;
+document.write(X);
+{
+    var X=33;   // example of var keyword which does not have block scope so that variable can be accessed from outside the block
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+// return statement that stops the execution of a funciton and returns a value from that function
+function reFunction() {
+    return Math.PI;
+}
+
+document.getElementById("pi").innerHTML = reFunction();
+
