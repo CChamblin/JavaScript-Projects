e1198b2cc50731be0aded4222dd8077fec85b6ba
Chris Chamblin <chamblin145@gmail.com>
2021-07-02 19:18:52 -0700

    JavaScript project updates

--- Javascript/JavaScript_1.js
+++ Javascript/JavaScript_1.js
@@ -7,22 +7,44 @@ function Color_Function() {
             Color_Output = "Red" + Color_String;
         break;
         case "Yellow":
-            Color_Output - "Yellow" + Color_String;
+            Color_Output = "Yellow" + Color_String;
         break;
         case "Green":
-            Color_Output - "Green" + Color_String;
+            Color_Output = "Green" + Color_String;
         break;
         case "Blue":
-            Color_Output - "Blue" + Color_String;
+            Color_Output = "Blue" + Color_String;
         break;
         case "Pink":
-            Color_Output - "Pink" + Color_String;
+            Color_Output = "Pink" + Color_String;
         break;
         case "Purple":
-            Color_Output - "Purple" + Color_String;
+            Color_Output = "Purple" + Color_String;
         break;
         default:
         Color_Output = "Please enter a color exactly as written on the above list.";
     }
     document.getElementById("Output").innerHTML = Color_Output;
 }
+
+//Get element by class name method
+function Hello_World_Function() {
+    var A = document.getElementByClassName("Click");
+    A[0].innerHTML = "The text has changed!";
+}
+
+//JavaScript used to add a circle to the HTML canvas element
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createLinearGradient(0,50,170,60);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "orange");
+
+ctx.fillStyle = grd;
+ctx.fillRect(0,0,250,175);
+
+ctx.beginPath();
+ctx.arc(125, 87.5, 80, 0, 2 * Math.PI);
+ctx.stroke();
+
