858ae5aa8b075df2b649aebf9424f89e71fad138
Chris Chamblin <Ishii.c71@gmail.com>
2021-08-17 22:21:13 -0700

    Selector challenge

--- JQuery/jQueryChallenge.js
+++ JQuery/jQueryChallenge.js
@@ -1,11 +1,27 @@
 // element selector
 $(document).ready(function(){
-    $("button").click(function(){
-        $("p").hide();
+    $("button1").click(function(){
+        $("h3").hide();
     });
 });
+
 // #id selector
+$(document).ready(function(){
+    $("button2").click(function(){
+        $("#test").hide();
+    });
+});
 
 // .class selector
+$(document).ready(function(){
+    $("button3").click(function(){
+        $(".test").hide();
+    });
+});
 
 // $("ul li:first-child") selector
+$(document).ready(function(){
+    $("button4").click(function(){
+        $("ul li:first-child").hide();
+    });
+});
