d26cc3eecb8662684133d67f72775a218a35027b
Chris Chamblin <Ishii.c71@gmail.com>
2021-08-17 20:56:39 -0700

    jQuery challanges element selector

---
+++ JQuery/jQueryChallenge.js
@@ -0,0 +1,11 @@
+// element selector
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+});
+// #id selector
+
+// .class selector
+
+// $("ul li:first-child") selector
