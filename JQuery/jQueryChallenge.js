b468e8d0c644d8e3f4b82058007d44a28b76386d
Chris Chamblin <Ishii.c71@gmail.com>
2021-10-09 16:43:26 -0700

    jQuery Challenges
    Update jQuery challenges file and upload Simon Says game.

--- JQuery/jQueryChallenge.js
+++ JQuery/jQueryChallenge.js
@@ -1,27 +1,196 @@
-// element selector
+// Selector
+
+// Element Selector
 $(document).ready(function(){
-    $("button1").click(function(){
-        $("h3").hide();
+    $("#element").click(function(){
+        $("#elementTest").hide();
     });
 });
 
-// #id selector
+// Id Selector
 $(document).ready(function(){
-    $("button2").click(function(){
-        $("#test").hide();
+    $("#idSelector").click(function(){
+        $("#idTest").hide();
     });
 });
 
-// .class selector
+// Class Selector
 $(document).ready(function(){
-    $("button3").click(function(){
-        $(".test").hide();
+    $("#classTest").click(function(){
+        $(".testA").hide();
     });
 });
 
 // $("ul li:first-child") selector
 $(document).ready(function(){
-    $("button4").click(function(){
+    $("#firstChild").click(function(){
         $("ul li:first-child").hide();
     });
 });
+
+// Selector
+
+
+// Events
+
+/*  Document Ready Event
+
+$(document).ready(function(){
+    jQuery method goes here
+});
+
+This prevents jQuery code from running before the
+document is finished loading.
+
+    Document Ready Event */
+
+// Click Method
+$(document).ready(function(){
+    $(".click").click(function(){
+      $(this).hide();
+    });
+  });
+
+$(document).ready(function(){
+    $(".dblclick").dblclick(function(){
+      $(this).hide();
+    });
+  });
+// Click Method
+
+// Hover Method - combination of mouseeneter() and mouseleave() methods
+$(document).ready(function(){
+    $("#hover").hover(function(){
+      alert("You entered hover paragraph!");
+    },
+    function(){
+      alert("Bye! You now left hover paragraph!")
+    });
+});
+// Hover Method
+
+// Focus & Blur Method - functions are executed when form gains and loses focus
+$(document).ready(function(){
+    $("input").focus(function(){
+        $(this).css("background-color", "blue");
+    });
+    $("input").blur(function(){
+        $(this).css("background-color", "purple");
+    });
+});
+// Focus & Blur Method
+
+// Events
+
+// Methods
+
+// Hide & Show
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("#hs").hide("slow");
+    });
+    $("#show").click(function(){
+        $("#hs").show("fast");
+    });
+});
+// Hide & show
+
+// Toggle
+$(document).ready(function(){
+    $("#toggle").click(function(){
+        $("#t").toggle(1700);
+    });
+});
+// Toggle
+
+// Methods
+
+// Effects
+
+// Fade Out - using on() method for both mouseeneter() and click()
+$(document).ready(function(){
+    $("#fadeout").click(function(){
+       $("#box1").fadeOut();
+       $("#box2").fadeOut("slow");
+       $("#box3").fadeOut(3000);
+    });
+});
+// Fade Out
+
+// Slide Down & Stop Animation
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideDown(1500);
+    });
+    $("#stop").click(function(){
+        $("#panel").stop();
+    });
+});
+// Slide Down & Stop Animation
+
+// Animate
+$(document).ready(function(){
+    $("#animate").click(function(){
+        var animation = $("#animation");
+        animation.animate({left: '100px'}, "slow");
+        animation.animate({fontSize: '3em'}, "fast", function(){
+            alert("The animation is finished");
+        });
+    });
+});
+// Animate
+
+// Chaining
+$(document).ready(function(){
+    $("#chain").click(function(){
+        $("#p1").css("color", "red").slideUp(2000).slideDown(2000);
+    });
+});
+// Chaining
+
+// Effects
+
+// DOM Manipulation
+
+// Get Content - Text & HTML
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        alert("Text: " + $("#test1").text());
+    });
+    $("#btn2").click(function(){
+        alert("HTML: " + $("#test1").html());
+    });
+});
+// Get Content - Text & HTML
+
+// Get Content - Attribute
+$(document).ready(function(){
+    $("#attr").click(function(){
+        alert($("#w3s").attr("href"));
+    });
+});
+// Get Content - Attribute
+
+// Set Content - Text & HTML & Value
+$(document).ready(function(){
+    $("#btn3").click(function(){
+        $("#test3").text("Hello world!");
+    });
+    $("#btn4").click(function(){
+        $("#test4").html("<b>Hello world!</b>");
+    });
+    $("#btn5").click(function(){
+        $("#test5").val("Dolly Duck");
+    });
+})
+// Set Content - Text & HTML & Value
+
+// Set Content - Attribute
+$(document).ready(function(){
+    $("#setattr").click(function(){
+        $("#w3s2").attr("href", "https://www.w3schools.com/jquery/");
+    });
+});
+// Set Content - Attribute
+
+// DOM Manipulation
--- Weather_App_Project/weather.js
+++ Weather_App_Project/weather.js
@@ -14,18 +14,79 @@ const iconValue = {
     PARTLY_CLOUDY_NIGHT: 'partly-cloudy-night'
 }
 
-// Find the lat and long of the users location
+//Fecth the weather from the dark ski api
+function fetchWeatherReport(apiKey, latitude, longitude) {
+
+    //To avoid the cors issue you need to run through a proxy or make the call server side
+    var DsProxyLink = 'https://cors-anywhere.herokuapp.com/';
+    var DsApiLink = `${DsProxyLink}https://api.darksky.net/forecast/${apiKey}/${latitude},${longitude}?exclude=minutely,alerts,flags`;
+
+    fetch(DsApiLink)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            //Work with JSON data here
+            var resultsHTML = "";
+            var tableHTML = "";
+            var summary = data.currently.summary;
+            var temperature = data.currently.temperature;
+            var icon = data.currently.icon;
+            var precipProbability = data.currently.precipProbability;
+            var humidity = data.currently.humidity;
+            var windSpeed = data.currently.windSpeed
+            var ts = new Date(data.currently.time * 1000);
+            var forecastDate = `${wDay[ts.getDay()]} ${wMonth[ts.getMonth()]} ${ts.getDate()}`
+
+            //Set values for the current conditions
+            //document.getElemenyById("location").innerHTML = name;
+            document.getElementById("dayTime").innerHTML = forecastDate;
+            document.getElementById("summary").innerHTML = summary;
+            document.getElementById("currentTemp").innerHTML = `${Math.round(temperature)}&deg`;
+            document.getElementById("weatherIcon").src = getICON(icon);
+            document.getElementById("perciptation").innerHTML = `Precipitation ${precipProbability*100}%`;
+            document.getElementById("humidty").innerHTML = `Humidity ${Math.round(humidity*100)}%`;
+            document.getElementById("wind").innerHTML = `Winds ${Math.round(windSpeed)} mph`;
+
+            //Render the forcasts tabs
+            document.getElementById("dailyForecast").innerHTML = renderWeeklyForecast(data.daily);
+            document.getElementById("weeklyForecast").innerHTML = renderDailyForecast(data.hourly);
+        })
+        
+}
+
+function fetchLocation(apiKey, latitude, longitude){
+
+    // You don't need a proxy but you need to secure your key in the google developer console.
+    var googleApiLink = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`;
+
+    fetch(googleApiLink)
+    .then(response => {
+        return response.json()
+    })
+    .then(data =>{
+        //Work with JSON data here
+        document.getElementById("location").innerHTML = data.results[4].formatted_address;
+    })
+    .catch (err =>{
+        throw ('Sorry, an error occured ${err}');
+    })
+
+}
+
+//Find the lat and long of the users location
 function intiGeoLocation() {
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, fail)
+    if (navigator.geolocation) {
+        //Call getCurrentPosition with success and failure callbacks
+        navigator.geolocation.getCurrentPosition(success, fail);
     }
-    else{
-        alert("Sorry, your browser does not support geolocation services.")
+    else {
+        alert("Sorry, your browser does not support geolocation services.");
     }
 }
 
-function success(position){
+function success(position) {
     //ADD your keys here. My keys are located in a key.js file but are not included in the sample code for security reasons.
     //var dsKey = "";
     //var googleApiKey= "";
@@ -33,3 +94,8 @@ function success(position){
     fetchWeatherReport(dsKey, position.coords.latitude, position.coords.longitude)
 } 
 
+function fail() {
+
+    //You could default to your favorite city like Kernersville, NC the home of Coder Foundry!
+    alert("Sorry, your browser does not support geolocation services.");
+}
